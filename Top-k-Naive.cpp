b9312951ea761b3f76e1299c884e4d1c753c0825
iamjoey123 <h124728193@gmail.com>
2019-11-12 12:01:09 -0500

    Just add a little dataString
    From Monday to Sunday

--- Top-k-Naive.cpp
+++ Top-k-Naive.cpp
@@ -1,5 +1,4 @@
- 
-#include<bits/stdc++.h>
+#include<stdc++.h>
 #include <string>
 #include <map>
 
@@ -36,52 +35,50 @@ int editDist(string str1, string str2, int m, int n)
 	);
 }
 
-multimap<int,string> subString(string str, string query, int n, int k)  
-{ 
-    multimap <int, string> tempmap;
-    string temp = "";
-    // Pick starting point 
-    for (int len = 1; len <= n; len++)  
-    {     
-        // Pick ending point 
-        for (int i = 0; i <= n - len; i++)  
-        { 
-            //  Print characters from current 
-            // starting point to current ending 
-            // point.   
-            int j = i + len - 1;             
-            for (int k = i; k <= j; k++)  
-                temp += str[k]; 
-	    if(tempmap.size() < k)
-		tempmap.insert(pair<int, string>(editDist(temp, query, temp.length(), query.length()), temp));
-	    else
-	    {
-		int dist = editDist(temp, query, temp.length(), query.length());
-		auto itr = --tempmap.end();
-		if(itr->first > dist)
+multimap<int, string> subString(string str, string query, int n, int k)
+{
+	multimap <int, string> tempmap;
+	string temp = "";
+	// Pick starting point 
+	for (int len = 1; len <= n; len++)
+	{
+		// Pick ending point 
+		for (int i = 0; i <= n - len; i++)
 		{
-			tempmap.erase(itr);
-			tempmap.insert(pair<int, string>(dist, temp));
+			//  Print characters from current 
+			// starting point to current ending 
+			// point.   
+			int j = i + len - 1;
+			for (int k = i; k <= j; k++)
+				temp += str[k];
+			if (tempmap.size() < k)
+				tempmap.insert(pair<int, string>(editDist(temp, query, temp.length(), query.length()), temp));
+			else
+			{
+				int dist = editDist(temp, query, temp.length(), query.length());
+				auto itr = --tempmap.end();
+				if (itr->first > dist)
+				{
+					tempmap.erase(itr);
+					tempmap.insert(pair<int, string>(dist, temp));
+				}
+			}
+			temp = "";
 		}
-	    }	
-	    temp = "";
-        } 
-    } 
+	}
 
-    return tempmap;
-} 
-
+	return tempmap;
+}
 int main()
 {
-	// your code goes here 
-	string str1 = "saturdab";
-	string str2 = "saturday";
+	string dataString[7] = { "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday" };
+	string query = "saturdab";
 	int k = 3;
-	multimap<int, string> substrings = subString(str1, str2, str1.length(), k);
-	for(auto i = substrings.begin(); i != substrings.end(); i++)
-		cout << i->second << ": " << i->first << endl;
-
-	//cout << editDist(str1, str2, str1.length(), str2.length());
-
+	for (int i = 0; i < 7; i++) {
+		cout << "The substring of " + dataString[i] +"\n";
+		multimap<int, string> substrings = subString(dataString[i], query, dataString[i].length(), k);
+		for (auto i = substrings.begin(); i != substrings.end(); i++)
+			cout << i->second << ": " << i->first << endl;
+	}
 	return 0;
 }
